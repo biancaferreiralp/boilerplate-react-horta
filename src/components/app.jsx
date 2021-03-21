68f9b83df5960c5ff5227629b50f150bca9e13ee
rafaelrocha <rafaelmachadodarocha@gmail.com>
2021-03-21 13:09:38 -0300

    creating boilerplate

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import giphy from 'giphy-api';
+//const giphy = require('giphy-api')({
+//  apiKey: 'fEeIPT8WDQiwgmV09yb4FPyEfTw11ghF',
+//  https: true
+//});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">  
+        </div>
+        <div className="right-scene">
+        </div>
+      </div> 
+    )
+  }
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx'
+
+import '../assets/stylesheets/application.scss';
+
+//giphy-api config
+
+//const giphy = require('giphy-api')({
+//  apiKey: 'fEeIPT8WDQiwgmV09yb4FPyEfTw11ghF',
+//  https: true
+//});
+
+//search = (query) => { 
+//  giphy.search({
+//    q: query,
+//    rating: 'g',
+//    limit: 10
+//  }, (error, response) => {
+//    do something with state
+//  })
+//};
+
+
